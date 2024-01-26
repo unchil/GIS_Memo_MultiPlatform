058d61f0434b178a7770dc27303bb4e5ac0d9d68
unchil <yeopo77@icloud.com>
2024-01-26 20:12:25 +0900

    Initial commit

---
+++ iosApp/iosApp/Collector.swift
@@ -0,0 +1,30 @@
+//
+//  Collecter.swift
+//  iosApp
+//
+//  Created by 여운칠 on 1/26/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import Foundation
+import shared
+
+class Collector<T> : Kotlinx_coroutines_coreFlowCollector {
+
+
+    let callback:(T) -> Void
+
+
+    init(callback: @escaping (T) -> Void) {
+        self.callback = callback
+    }
+     
+    
+    func emit(value: Any?, completionHandler: @escaping (Error?) -> Void) {
+        callback(value as! T)
+        completionHandler(nil)
+    }
+ 
+}
+
+
