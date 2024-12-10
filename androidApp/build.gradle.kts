17d73e25e419beb8e12dc7dd24db6e4d836a95d7
unchil <yeopo77@icloud.com>
2024-12-10 14:20:06 +0900

    Initial commit

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.unchil.gismemo_multiplatform.android"
         minSdk = 33
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
